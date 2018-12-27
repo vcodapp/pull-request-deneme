f0738e8f64200c84ede68d4ed4819840addf1f21
Vcode <volkanciloglu@gmail.com>
2018-12-27 16:11:38 +0300

    I change uppercase hungry

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var hungry = prompt('Are you hungry?');
 
-if (hungry == "Yes" || hungry == "y") {
+if (hungry == "Yes" || hungry == "y" || hungry == "Y") {
   return console.log('Eat something');
 } else {
   return console.log('Go to work');
