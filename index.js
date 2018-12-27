4f7b85126db86271671512190a123c3124e8b741
Vcode <volkanciloglu@gmail.com>
2018-12-27 16:06:17 +0300

    added hungry or statement 'y'

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var hungry = prompt('Are you hungry?');
 
-if (hungry == "Yes") {
+if (hungry == "Yes" || hungry == "y") {
   return console.log('Eat something');
 } else {
   return console.log('Go to work');
