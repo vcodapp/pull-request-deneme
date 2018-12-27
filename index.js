92b0a4f2b9fbe04cbce54373f0a4654ef0733cd3
Vcode <volkanciloglu@gmail.com>
2018-12-27 16:43:22 +0300

    Return error

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var hungry = prompt('Are you hungry?');
 var element = document.getElementsByTagName('P')[0];
 
-if (hungry == "Yes" || hungry == "y" || hungry == "Y") {
+iof(hungry == "Yes" || hungry == "y" || hungry == "Y") {
   element.innerHTML = "Eat something."
 } else {
-  element.innerHTML = "Go to work."
+  element.innerHTML = "Go to work.";
 }
